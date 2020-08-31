85d9738cd0e564f15bc7513097a3f6d3d5acbc36
Cameron Hyde <pluckthechicken@hotmail.co.uk>
2020-08-31 17:58:32 +1000

    Added pairwise BLAST

--- blast/blast.py
+++ blast/blast.py
@@ -1,8 +1,10 @@
 """A wrapper for the NCBI BLAST tool."""
 
 import os
+import uuid
 import logging
 import subprocess
+from .parse import BlastResult
 
 logger = logging.getLogger('root')
 
@@ -10,7 +12,7 @@ logger = logging.getLogger('root')
 def blast(algorithm, queryfile, db):
     """Run a blast search, check outcome and return parsed result."""
     name = os.path.basename(queryfile)
-    outfilename = 'blast/blastout_%s.xml' % name
+    outfilename = 'blast/blastout_%s.xml' % name.rsplit('.', 1)[0]
 
     if not os.path.exists(f"{ db }.nin"):
         raise FileNotFoundError(f'BLAST DB "{ db }" not found')
@@ -33,3 +35,29 @@ def blast(algorithm, queryfile, db):
         raise exc
 
     return outfilename
+
+
+def pairwise_blast(algorithm, query, subject):
+    """BLAST align two sequences and return BlastResult instance."""
+    queryfile = os.path.join('blast', uuid.uuid4().hex + ".fas")
+    subjectfile = os.path.join('blast', uuid.uuid4().hex + ".fas")
+    outfile = os.path.join('blast', uuid.uuid4().hex + ".xml")
+    args = (
+        algorithm,
+        '-query', queryfile,
+        '-subject', subjectfile,
+        '-out', outfile,
+        '-outfmt', '5',
+        "-evalue", '0.01',
+        "-max_target_seqs", '10',
+    )
+    try:
+        subprocess.run(args, check=True)
+    except subprocess.CalledProcessError as exc:
+        logger.error(exc.stderr)
+        raise exc
+    finally:
+        os.remove(queryfile)
+        os.remove(subjectfile)
+
+    # Parse out alignment identity % and return
